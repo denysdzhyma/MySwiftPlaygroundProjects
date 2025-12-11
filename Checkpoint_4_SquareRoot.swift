a0cdbbd905a69c152e56d53d0a2da5111d7978e2
Denys <80990259+denysdzhyma@users.noreply.github.com>
2025-12-10 21:04:59 -0500

    Added SquareRoot WITHOUT using sqrt() or similar

---
+++ Checkpoint_4_SquareRoot.swift
@@ -0,0 +1,41 @@
+//
+//  main.swift
+//  Checkpoint_4_SquareRoot
+//
+//  Created by Denys on 2025-12-10.
+//
+
+import Cocoa
+
+enum SquareRootErrors: Error {
+    case outOfBounds
+    case noRootFound
+}
+
+func squareRootFinder(_ number: Int) throws -> Int {
+    
+    if number <= 0 || number > 10_000 {
+        throw SquareRootErrors.outOfBounds
+    }
+    
+    for findSquareRoot in 1...10_000 {
+        if findSquareRoot * findSquareRoot == number {
+            return findSquareRoot
+        } else if findSquareRoot >= 10_001 {
+            throw SquareRootErrors.noRootFound
+        }
+    }
+    return number
+}
+
+// TESTING.
+do {
+    try squareRootFinder(25)
+} catch SquareRootErrors.outOfBounds {
+    print("Error! Out of Range.")
+} catch SquareRootErrors.noRootFound {
+    print("Error! No Root Found.")
+} catch {
+    print("Error! Unknown.")
+}
+
